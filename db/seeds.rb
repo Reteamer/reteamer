f04adc1cb86cdbf39712dd9d8d94f348759ab3c5
Will Read <will.read@gmail.com>
2021-10-21 15:25:19 -0700

    More messages

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,11 @@
 require_relative "./seeds/30rock"
 
+p "Run ing seeds..."
 if Rails.env.production?
   raise "Can't run seeds on production DB because it'll destroy data"
 end
+
+p "Clearing the DB..."
 
 ActsAsTenant.without_tenant do
   User.destroy_all
@@ -11,6 +14,8 @@ ActsAsTenant.without_tenant do
   Team.destroy_all
   Assignment.destroy_all
 end
+
+p "Adding 30 Rock data..."
 
 thirtyRockUser = User.create!(first_name: "30", last_name: "Rock", email: "demo@thirtyrock.com", admin: true, password: 'password', password_confirmation: 'password', :terms_of_service => true)
 
