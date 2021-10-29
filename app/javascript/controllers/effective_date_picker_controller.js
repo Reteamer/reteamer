05fb78607c33ad86a9efa5080f5b6fb8f0415972
Will Read <will.read@gmail.com>
2021-10-29 11:58:09 -0700

    Date picker better accounts for date navigation

--- app/javascript/controllers/effective_date_picker_controller.js
+++ app/javascript/controllers/effective_date_picker_controller.js
@@ -2,11 +2,13 @@ import { Controller } from "stimulus"
 import { emitCompleteChangeEvent } from "../event_emitter";
 
 export default class extends Controller {
-  static targets = [ "selectedDate" ]
+  static targets = [ "selectedDate", "selectedDateInput", "otherDateInput" ]
 
   handleDatePicked(event) {
     this.selectedDate = event.detail.newDate;
     this.selectedDateTarget.innerHTML = this.selectedDate;
+    this.selectedDateInputTarget.value = this.selectedDate;
+    this.otherDateInputTarget.value = this.selectedDate;
   }
 
   cancelChange() {
@@ -19,6 +21,7 @@ export default class extends Controller {
     if (newDate == "other") {
       newDate = document.getElementById("other_effective_date").value
     }
+    console.error("=============>", newDate);
     emitCompleteChangeEvent(newDate)
   }
 
