a2c6e5e79033ca9f5fb2367cadc5feb5c91c5e0c
Will Read <will.read@gmail.com>
2021-11-10 00:21:17 -0800

    Diagnostics for PR app
    - it seems to think we shouldn't be able to reset the data for... reasons

--- config/initializers/feature_flags.rb
+++ config/initializers/feature_flags.rb
@@ -1,7 +1,9 @@
 if Rails.env.development? || Rails.env.test?
   ENV["ALLOW_SIGN_UP"] = "true"
-  ENV["CAN_RESET_DATA"] = "true"
+  # ENV["CAN_RESET_DATA"] = "true"
 end
+
+puts "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", ENV.fetch("CAN_RESET_DATA", "false")
 
 Rails.configuration.allow_sign_up = ENV.fetch("ALLOW_SIGN_UP", "true").to_bool
 Rails.configuration.can_reset_data = ENV.fetch("CAN_RESET_DATA", "false").to_bool
