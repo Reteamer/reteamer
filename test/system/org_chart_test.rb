2627b915abbd4d89ba3d8018f1f466ea47e92e21
Will Read <will.read@gmail.com>
2021-11-09 16:15:46 -0800

    Adds a test for clicking on the slider and expecting the UI to update

--- test/system/org_chart_test.rb
+++ test/system/org_chart_test.rb
@@ -1,11 +1,17 @@
 require "application_system_test_case"
 
 class OrgChartTest < ApplicationSystemTestCase
-  test "can navigate with date input" do
+  setup do
     thirty_rock_user = User.create!(first_name: "30", last_name: "Rock", email: "demo@thirtyrock.com", admin: true, password: "password", password_confirmation: "password", terms_of_service: true)
     peopleOfThirtyRock(thirty_rock_user.accounts.first)
     login_as(thirty_rock_user)
+  end
 
+  teardown do
+    logout
+  end
+
+  test "The date navigator is initialized correctly" do
     visit org_chart_path
     within("date-navigator") do
       assert_selector "input[value='#{Date.today.iso8601}']"
@@ -19,6 +25,21 @@ class OrgChartTest < ApplicationSystemTestCase
       find('.selected-date-marker', visible: :all).hover
       selected_date = find(".cursor-date", visible: :all).text
       assert_equal(selected_date, Date.today.iso8601)
+    end
+  end
+
+  test "Using the slider" do
+    visit org_chart_path
+    within("date-navigator") do
+      some_place_in_the_future = all('.change-counts', visible: :all)[2]
+      x = some_place_in_the_future.rect.x.to_i
+      y = 20
+      take_screenshot
+      find(".mouse-catcher").click(x: x, y: y)
+      hovered_date = find(".cursor-date", visible: :all).text
+      assert_not_empty(hovered_date)
+      assert_equal(find("input").value, hovered_date)
+      assert_equal(URI.parse(current_url).query, "effective_date=#{hovered_date}")
     end
   end
   # test "can navigate with date navigator"
