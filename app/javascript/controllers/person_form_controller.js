d5f5f328dc0422a151f37124a469b4d673701081
Will Read <will.read@gmail.com>
2021-11-10 16:07:41 -0800

    Adds transitions for new person form

---
+++ app/javascript/controllers/person_form_controller.js
@@ -0,0 +1,19 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = [ "sectionOne", "sectionTwo", "sectionThree" ]
+
+  handleDatePicked(event) {
+    event.preventDefault();
+    this.sectionOneTarget.classList.add("hidden")
+    this.sectionTwoTarget.classList.remove("hidden")
+  }
+
+  handlePersonTypePicked(event) {
+    this.sectionTwoTarget.classList.add("hidden")
+    this.sectionThreeTarget.classList.remove("hidden")
+  }
+
+  connect() {
+  }
+}
