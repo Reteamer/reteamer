7574e4f9bbce874686d5ae4dcf204db9d456a279
Will Read <will.read@gmail.com>
2021-11-11 21:44:23 -0800

    Completes the styleguide for the effective_date_fields
    #41

--- app/javascript/controllers/effective_date_fields_controller.js
+++ app/javascript/controllers/effective_date_fields_controller.js
@@ -2,21 +2,46 @@ import { Controller } from "@hotwired/stimulus"
 import dayjs from "dayjs";
 
 export default class extends Controller {
-  static targets = [ "hiddenInput", "selectedDate", "selectedDateInput", "otherDateInput" ]
+  static targets = [
+    "hiddenInput",
+    "selectedDate",
+    "selectedDateInput",
+    "otherDateRadio",
+    "otherDateInput"
+  ]
+
+  static values = {
+    selectedDate: String
+  }
 
   handleDatePicked(event) {
-    console.error("=============>", "handling");
-    this.selectedDate = event.detail.newDate;
-    this.selectedDateTarget.innerHTML = dayjs(this.selectedDate).format("ddd, MMM D, YYYY");
-    this.selectedDateInputTarget.value = this.selectedDate;
-    this.otherDateInputTarget._flatpickr.setDate(this.selectedDate);
+    const newDate = event.detail.newDate;
+    this.selectedDateTarget.innerHTML = dayjs(newDate).format("ddd, MMM D, YYYY");
+    this.selectedDateInputTarget.value = newDate;
+    this.otherDateInputTarget._flatpickr.setDate(newDate);
+    this.setHiddenInputValue(newDate);
+  }
+
+  setHiddenInputValue(value) {
+    this.hiddenInputTarget.value = value
+    this.hiddenInputTarget.dispatchEvent(new Event('change'));
   }
 
   handleRadioChange(event) {
-    this.hiddenInputTarget.value = event.target.value
+    if(event.target.value === "other") {
+      this.setHiddenInputValue(this.otherDateInputTarget.value);
+    } else {
+      this.setHiddenInputValue(event.target.value);
+    }
+  }
+
+  handleOtherChange(event) {
+    if(this.otherDateRadioTarget.checked) {
+      this.setHiddenInputValue(this.otherDateInputTarget.value);
+    }
   }
 
   connect() {
-    this.selectedDate = null;
+    this.setHiddenInputValue(this.selectedDateValue);
   }
 }
