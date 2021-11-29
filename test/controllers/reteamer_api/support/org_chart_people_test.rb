addd9d2218b57630e0873a9628f6717621f2dbee
Will Read <will.read@gmail.com>
2021-11-29 15:39:15 -0800

    Fixes broken spec

--- test/controllers/reteamer_api/support/org_chart_people_test.rb
+++ test/controllers/reteamer_api/support/org_chart_people_test.rb
@@ -2,36 +2,34 @@ require "test_helper"
 
 class OrgChartPeopleTest < ActiveSupport::TestCase
   def setup
-    # Do nothing
-  end
+    @today = Date.today
 
-  def teardown
-    # Do nothing
+    @main_entry = ActsAsProposable.with_proposal(proposals(:main)) do
+      Entry.create(effective_at: @today, versionable: People::Person.new(first_name: "Main Person"))
+    end
+
+    @proposed_entry = ActsAsProposable.with_proposal(proposals(:plan_b)) do
+      Entry.create(effective_at: @today, versionable: People::Person.new(first_name: "Proposed Person"))
+    end
+
+    @other_entry = ActsAsProposable.with_proposal(proposals(:plan_c)) do
+      Entry.create(effective_at: @today, versionable: People::Person.new(first_name: "Other Person"))
+    end
   end
 
   test "#org_chart returns people in the main plan and the specified plan" do
-    today = Date.today
-    main_entry = nil
-    proposed_entry = nil
-    other_entry = nil
     ActsAsProposable.with_proposal(proposals(:main)) do
-      main_entry = Entry.create(effective_at: today, versionable: People::Person.new(first_name: "Main Person"))
+      actual = ReteamerApi::Support::OrgChartPeople.org_chart(@today)
+      assert_includes(actual, @main_entry.versionable)
+      refute_includes(actual, @proposed_entry.versionable)
+      refute_includes(actual, @other_entry.versionable)
     end
+
     ActsAsProposable.with_proposal(proposals(:plan_b)) do
-      proposed_entry = Entry.create(effective_at: today, versionable: People::Person.new(first_name: "Proposed Person"))
+      actual = ReteamerApi::Support::OrgChartPeople.org_chart(@today)
+      assert_includes(actual, @main_entry.versionable)
+      assert_includes(actual, @proposed_entry.versionable)
+      refute_includes(actual, @other_entry.versionable)
     end
-    ActsAsProposable.with_proposal(proposals(:plan_c)) do
-      other_entry = Entry.create(effective_at: today, versionable: People::Person.new(first_name: "Other Person"))
-    end
-
-    actual = ReteamerApi::Support::OrgChartPeople.org_chart(Date.today, proposals(:plan_b).name)
-    assert(actual.include?(main_entry.versionable))
-    assert(actual.include?(proposed_entry.versionable))
-    assert(!actual.include?(other_entry.versionable))
-
-    actual = ReteamerApi::Support::OrgChartPeople.org_chart(Date.today, proposals(:main).name)
-    assert(actual.include?(main_entry.versionable))
-    assert(!actual.include?(proposed_entry.versionable))
-    assert(!actual.include?(other_entry.versionable))
   end
 end
