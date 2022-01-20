8dce43817581dfda4894168017def3551c444cec
Will Read <will.read@gmail.com>
2022-01-19 16:54:11 -0800

    Removes open reqs from the list of supervisors
    Closes #191

--- app/controllers/reteamer_api/supervisors_controller.rb
+++ app/controllers/reteamer_api/supervisors_controller.rb
@@ -4,7 +4,7 @@ module ReteamerApi
 
     def index
       selected_date = Date.parse(params.fetch(:effective_date, Date.today.iso8601))
-      @supervisors = Entry.find_for(selected_date, versionable_type: People::Person.name).map(&:versionable)
+      @supervisors = Entry.find_for(selected_date, versionable_type: People::Person.name).map(&:versionable).select { |v| !v.is_a?(People::OpenReq) }
     end
   end
 end
