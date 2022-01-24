63db2cf3780b685a4071721f44276d5aba993fd3
Will Read <will.read@gmail.com>
2022-01-24 14:50:05 -0800

    Fixes some 500 errors in the logs

--- app/javascript/controllers/team_chart_controller.js
+++ app/javascript/controllers/team_chart_controller.js
@@ -119,7 +119,7 @@ export default class TeamChartController extends Controller {
                 data-team-buttons-team-key-param="${d.data.id}"
               >
                 <circle r="10" cx="10" cy="10"></circle>
-                <image xlink:href="trash.svg" x="4" y="4" height="12" width="12"></image>
+                <image xlink:href="/trash.svg" x="4" y="4" height="12" width="12"></image>
               </g>
               <g class="team-button edit-team" 
                 transform="translate(${d.width - 48},${d.height - 24})"
@@ -127,7 +127,7 @@ export default class TeamChartController extends Controller {
                 data-team-buttons-team-param="${encodeURIComponent(JSON.stringify(d.data))}" 
               >
                 <circle r="10" cx="10" cy="10"></circle>
-                <image xlink:href="pencil-solid.svg" x="4" y="4" height="12" width="12"></image>
+                <image xlink:href="/pencil-solid.svg" x="4" y="4" height="12" width="12"></image>
               </g>
             </g>
           </g>
