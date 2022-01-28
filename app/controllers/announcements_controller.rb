57e188f129884b63f32c717dbcaea599fb41c30f
Will Read <will.read@gmail.com>
2022-01-28 14:01:25 -0800

    WIP: adds the left nav to another controller

--- app/controllers/announcements_controller.rb
+++ app/controllers/announcements_controller.rb
@@ -1,5 +1,6 @@
 class AnnouncementsController < ApplicationController
   before_action :mark_as_read, if: :user_signed_in?
+  layout "application_left"
 
   def index
     @pagy, @announcements = pagy(Announcement.order(published_at: :desc))
